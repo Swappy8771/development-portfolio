84e10bdb78701337f1009fe2299231b27a0c059f
Swappy8771 <swapnilpatil8771@gmail.com>
2026-03-19 15:36:28 +0530

    build changes

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,12 @@
+import { resolve } from "node:path";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  turbopack: {
+    root: resolve(process.cwd()),
+  },
 };
 
 export default nextConfig;
